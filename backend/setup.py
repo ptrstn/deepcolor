f2ae210b33468e9ddc3cec325463dc776fb033f5
Peter Stein <peterstein@protonmail.com>
2020-05-10 09:07:32 +0200

    Release backend version 0.1.0

--- backend/setup.py
+++ backend/setup.py
@@ -2,10 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="backend",
-    version="0.0.1",
+    version="0.1.0",
     description="A website where you can upload a black and white image to get a color image",
     url="https://github.com/INF-HS-KL-BEGGEL/DL-SS20-T1-image-col",
-    author="Sebastian Dauenhauer, Eric Gaida, Peter Stein",
     packages=find_packages(),
     install_requires=["django>=3,<4", "djangorestframework", "Pillow", "deepcolor"],
 )
